eb88b7c289d985b55b549767a2248a84f5ae7dc4
daniele.minatto <daniele.minatto@gmail.com>
2024-12-31 00:42:57 -0300

    feat: add list and create to api

--- src/presentation/controllers/parking.controller.ts
+++ src/presentation/controllers/parking.controller.ts
@@ -1,10 +1,40 @@
-class ParkingController {
-  constructor() {
+import { Request, Response } from 'express'
+import ParkingCreateRequest from '../../application/dtos/parkingCreateRequest.dto'
+import IParkingEntranceUsecase from '../../application/interfaces/usecases/parkingEntrance.interface'
+import IParkingExitUsecase from '../../application/interfaces/usecases/parkingExit.interface'
+import IParkingListUsecase from '../../application/interfaces/usecases/parkingList.interface'
+import IParkingPaymentUsecase from '../../application/interfaces/usecases/parkingPayment.interface'
+import ParkingEntranceUsecase from '../../domain/usecases/parking/entrance/parkingEntrance.usecase'
+import ParkingExitUsecase from '../../domain/usecases/parking/exit/parkingExit.usecase'
+import ParkingListUsecase from '../../domain/usecases/parking/list/parkingList.usecase'
+import ParkingPaymentUsecase from '../../domain/usecases/parking/payment/parkingPayment.usecase'
 
-    listByPlate(req, res) {
-      return ""
-    }
+class ParkingController {
+  private parkingEntranceUsecase: IParkingEntranceUsecase
+  private parkingExitUsecase: IParkingExitUsecase
+  private parkingListUsecase: IParkingListUsecase
+  private parkingPaymentUsecase: IParkingPaymentUsecase
+
+  constructor() {
+    this.parkingEntranceUsecase = new ParkingEntranceUsecase()
+    this.parkingExitUsecase = new ParkingExitUsecase()
+    this.parkingListUsecase = new ParkingListUsecase()
+    this.parkingPaymentUsecase = new ParkingPaymentUsecase()
+  }
+
+  create(data: ParkingCreateRequest, res: Response) {
+    return this.parkingEntranceUsecase
+      .execute(data)
+      .then((a) => res.status(200).json({ result: a }))
+      .catch((err) => res.status(400).json({ result: err }))
+  }
+
+  list(plate: string, res: Response) {
+    return this.parkingListUsecase
+      .execute(plate)
+      .then((crushs) => res.status(200).json({ result: crushs }))
+      .catch((err) => res.status(400).json({ result: err }))
   }
 }
 
-export default ParkingController;
+export default ParkingController
--- src/presentation/routes/index.routes.ts
+++ src/presentation/routes/index.routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express'
+import ParkingCreateRequest from '../../application/dtos/parkingCreateRequest.dto'
 import ParkingController from '../controllers/parking.controller'
 
 const routes = Router()
@@ -10,16 +11,33 @@ routes.get('/healthcheck', (req: Request, res: Response) => {
 })
 
 routes.get('/parking/:plate', (req: Request, res: Response) => {
-  res.json({ message: 'Wow! parking list' })
+  var plate = req.params.plate as unknown as string
+  controller.list(plate, res)
 })
 
 routes.post('/parking', (req: Request, res: Response) => {
-  res.json({ message: 'Wow! parking post' })
+  /*
+  #swagger.tags = ['Parking']
+  #swagger.summary = 'Create a entrance'
+  #swagger.description = 'This endpoint will create a entrance in the parking lot'
+  #swagger.requestBody = {
+                required: true,
+                content: {
+                    "application/json": {
+                        schema: {
+                            $ref: "#/components/schemas/parkingCreateRequest"
+                        }
+                    }
+                }
+            }
+*/
+  var body = req.body as unknown as ParkingCreateRequest
+  controller.create(body, res)
 })
 
 routes.put('/parking/:id/out', (req: Request, res: Response) => {
   var id = req.params.id as unknown as number
-  controller.exit(id, res)
+  // controller.exit(id, res)
 })
 
 routes.put('/parking/:id/pay', (req: Request, res: Response) => {
