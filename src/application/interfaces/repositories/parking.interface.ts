e897ee3b000ebb595b2509adee25409330575af1
daniele.minatto <daniele.minatto@gmail.com>
2024-12-31 14:40:45 -0300

    refactor: repository data types

--- src/application/interfaces/repositories/parking.interface.ts
+++ src/application/interfaces/repositories/parking.interface.ts
@@ -1,13 +1,10 @@
 import ParkingCreateRequest from '../../dtos/parkingCreateRequest.dto'
 import ParkingEntity from '../../../domain/entities/parking.entity'
+import ParkingUpdateRequest from '../../dtos/parkingUpdateRequest.dto'
 interface IParkingRepository {
   list: (plate: string) => Promise<ParkingEntity[]>
   create: (item: ParkingCreateRequest) => Promise<ParkingEntity>
-  update: (
-    _id: number,
-    item: ParkingEntity,
-    callback: (error: any, result: any) => void
-  ) => void
+  update: (_id: string, item: ParkingUpdateRequest) => Promise<ParkingEntity>
   delete: (_id: number, callback: (error: any, result: any) => void) => void
 }
 
--- src/infrastructure/repositories/parking.repository.ts
+++ src/infrastructure/repositories/parking.repository.ts
@@ -4,29 +4,31 @@ import Parking from '../models/parking.model'
 import ParkingEntity from '../../domain/entities/parking.entity'
 import ParkingCreateRequest from '../../application/dtos/parkingCreateRequest.dto'
 import IParkingRepository from '../../application/interfaces/repositories/parking.interface'
+import ParkingUpdateRequest from '../../application/dtos/parkingUpdateRequest.dto'
 
 class ParkingRepository implements IParkingRepository {
   async create(item: ParkingCreateRequest): Promise<ParkingEntity> {
-    const newParking = new Parking(item)
-    newParking.save()
-    return mapper.mapAsync(newParking, Parking, ParkingEntity)
+    var data = new Parking(item)
+    data.save()
+    return mapper.mapAsync(data, Parking, ParkingEntity)
   }
 
   async list(plate: string): Promise<ParkingEntity[]> {
-    const data = await Parking.find({ plate })
+    var data = await Parking.find({ plate })
     return mapper.mapArrayAsync(data, Parking, ParkingEntity)
   }
 
-  update(
-    _id: number,
-    item: ParkingEntity,
-    callback: (error: any, result: any) => void
-  ): void {
-    Parking.findByIdAndUpdate(
+  async update(
+    _id: string,
+    item: ParkingUpdateRequest
+  ): Promise<ParkingEntity> {
+    var data = await Parking.findByIdAndUpdate(
       _id,
-      { $set: { ...item, updatedDate: new Date() } },
+      { $set: { ...item } },
       { new: true }
     )
+
+    return mapper.mapAsync(data, Parking, ParkingEntity)
   }
 
   delete(_id: number, callback: (error: any, result: any) => void): void {
